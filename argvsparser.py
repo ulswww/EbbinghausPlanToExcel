d3684e9af6fc806bcf7565fb09df330c4cbdc59e
ulswww <ulswww@gmail.com>
2020-02-08 18:26:06 +0800

    Create argvsparser.py

---
+++ argvsparser.py
@@ -0,0 +1,49 @@
+import sys
+from datetime import datetime
+import functools
+
+
+argv = sys.argv[1:]
+
+
+def get_arg_value(type):
+    argv_len = len(argv)
+    index = argv.index(type) if type in argv else -1
+    if index < 0 or index >= argv_len - 1:
+        return None
+    else:
+        value = argv[index+1]
+        return value
+
+
+def parse_date(value, default):
+    if not value:
+        return default
+    else:
+        return datetime.strptime(value, '%Y-%m-%d')
+
+
+def parse_int(value, default):
+    if not value:
+        return default
+    else:
+        return int(value)
+
+
+def parse_boolean(value, default):
+    if not value:
+        return default
+    else:
+        return bool(value)
+
+
+def parse_func(type, default):
+    valuestr = get_arg_value(type)
+    getterfunc = argv_getter.get(type)
+    return functools.partial(getterfunc, value=valuestr, default=default)
+
+
+argv_getter = {'-d': parse_date,
+               '-c': parse_int,
+               '-s': parse_int,
+               '-e': parse_boolean}
